99514833fbf163540b4c011a455837147546173d
ahuskano <huskanovicalen@gmail.com>
2014-03-23 00:16:07 +0100

    application for checking supported sensors

---
+++ SensorsTesting/app/src/main/java/com/alen/sensorstesting/app/MainActivity.java
@@ -0,0 +1,52 @@
+package com.alen.sensorstesting.app;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportedSensors();
+    }
+    private void getSupportedSensors(){
+        SensorManager sensorManager=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors=sensorManager.getSensorList(Sensor.TYPE_ALL);
+        for(Sensor s:sensors){
+            Log.d("sensors_info",s.getName());
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
